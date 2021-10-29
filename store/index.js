b999990d08d508dfc7ca6955a13736a0e8de8440
Jakub Ferenc <jakub@assemblage.cz>
2021-10-29 15:08:03 +0200

    Remove util functions from store

--- store/index.js
+++ store/index.js
@@ -5,293 +5,15 @@ this is where we will eventually hold the data for all of our posts
 import apiFactory from '../factories';
 import projectConfig from '../project.config';
 
-const normalizeUstrApiMediaObjectForWordpress = (soubor) => {
-
-  // http://parliament.ustrcr.cz/Api/Help/Api/GET-soubory_Limit_Stranka_Mime
-
-  soubor.url = soubor.URLNahled;
-  soubor.title = soubor.Poznamka;
-  soubor.mime_type = soubor.Mime;
-
-  return soubor;
-
-};
-
-const normalizeMediaAttrs = (item) => {
-
-  if (!item.sizes) {
-    if (item.media_details) {
-      item.sizes = item.media_details.sizes;
-    }
-  }
-
-  return item;
-
-};
-
-const getAdresaDruhHumanReadableName = (adresaDruhNumber) => {
-
-  if (adresaDruhNumber == 1) {
-    return 'Místo narození';
-  }
-
-  if (adresaDruhNumber == 2) {
-    return 'Adresa bydliště při zvolení';
-  }
-
-  if (adresaDruhNumber == 3) {
-    return 'Adresa bydliště při začátku mandátu';
-  }
-
-  if (adresaDruhNumber == 4) {
-    return 'Adresa bydliště při svatbě';
-  }
-
-  if (adresaDruhNumber == 5) {
-    return 'Adresa bydliště při úmrtí';
-  }
-
-  if (adresaDruhNumber == 6) {
-    return 'Místo hrobu';
-  }
-
-  if (adresaDruhNumber == 255) {
-    return 'Ostatní typ adresy';
-  }
-
-};
-
-const getAdresyProMapuForPoslanec = (poslanec) => {
-
-
-  let adresyPolozky = [];
-  let adresyMandaty = [];
-
-  if (poslanec.Adresy) {
-    adresyPolozky = [...adresyPolozky, ...poslanec.Adresy.map((adresa) => {
-
-      adresa.DruhNazev = getAdresaDruhHumanReadableName(adresa.Druh);
-
-      return adresa;
-
-    })];
-  }
-
-  if (poslanec.Mandaty && poslanec.Mandaty.length > 0) {
-
-
-    poslanec.Mandaty.forEach((mandat, index) => {
-
-      if (mandat.AdresyVykonMandatu) {
-        adresyMandaty = [...adresyMandaty, ...mandat.AdresyVykonMandatu
-          .filter(adresa => adresa.Druh != 1 && adresa.Druh != 5)
-          .map((adresa) => {
-          adresa._mandatId = mandat.Id;
-          adresa.Parlament = mandat.Parlament;
-          adresa.DatumZacatkuZobrazene = mandat.DatumZacatkuZobrazene;
-          adresa.DatumKonceZobrazene = mandat.DatumKonceZobrazene;
-          adresa.DruhNazev = getAdresaDruhHumanReadableName(adresa.Druh);
-          adresa.DruhTyp = 'AdresaVykonMandatu';
-          return adresa;
-        })];
-      }
-      if (mandat.AdresyZvoleni) {
-        adresyMandaty = [...adresyMandaty, ...mandat.AdresyZvoleni
-          .filter(adresa => adresa.Druh != 1 && adresa.Druh != 5)
-          .map((adresa) => {
-          adresa._mandatId = mandat.Id;
-          adresa.Parlament = mandat.Parlament;
-          adresa.DatumZacatkuZobrazene = mandat.DatumZacatkuZobrazene;
-          adresa.DatumKonceZobrazene = mandat.DatumKonceZobrazene;
-          adresa.DruhNazev = getAdresaDruhHumanReadableName(adresa.Druh);
-          adresa.DruhTyp = 'AdresaZvoleni';
-          return adresa;
-        })];
-      }
-
-    });
-
-    console.info("adresyMandaty", adresyMandaty);
-
-
-
-  }
-
-  return [...adresyPolozky, ...adresyMandaty];
-
-
-};
-
-const getCasovaOsaDataForPoslanec = (poslanec) => {
-
-  const randomColors = [];
-
-  let casovaOsaPolozky = [];
-
-  if (poslanec.DatumNarozeni) {
-
-    casovaOsaPolozky = [...casovaOsaPolozky, {
-
-      datum_udalosti: poslanec.DatumNarozeni.split("T")[0],
-      dulezita: true,
-      nazev_udalosti: `Datum narození ${poslanec.DatumNarozeniZobrazene}`,
-
-    }];
-
-  }
-
-
-  if (poslanec.Mandaty) {
-
-    const casovePolozkyMandatyCalculated = poslanec.Mandaty.reduce((acc, mandat, index) => {
-
-      if (mandat.DatumZacatku) {
-
-        acc.push({
-
-          datum_udalosti: mandat.DatumZacatku.split("T")[0],
-          dulezita: false,
-          nazev_udalosti: `Začátek mandátu (${mandat.DatumZacatkuZobrazene || 'Neuvedeno'})`,
-
-        });
-
-      }
-
-      if (mandat.DatumKonce) {
-
-        acc.push({
-
-          datum_udalosti: mandat.DatumKonce.split("T")[0],
-          dulezita: false,
-          nazev_udalosti: `Konec mandátu (${mandat.DatumKonceZobrazene || 'Neuvedeno'})`,
-
-        });
-
-      }
-
-
-      return acc;
-
-    }, []);
-
-    casovaOsaPolozky = [...casovaOsaPolozky,...casovePolozkyMandatyCalculated];
-
-
-  }
-
-
-  casovaOsaPolozky = casovaOsaPolozky.sort((a, b) => (a.datum_udalosti > b.datum_udalosti) ? 1 : (a.datum_udalosti < b.datum_udalosti ) ? -1 : 0);
-
-  if (poslanec.DatumUmrti) {
-
-    casovaOsaPolozky = [...casovaOsaPolozky, {
-
-      datum_udalosti: poslanec.DatumUmrti.split("T")[0],
-      dulezita: true,
-      nazev_udalosti: `Datum úmrtí ${poslanec.DatumUmrtiZobrazene}`,
-
-    }];
-
-  }
-
-  return casovaOsaPolozky;
-
-
-};
-
-const getFilterDataFromPoslanciAll = (poslanci) => {
-
-  // the values must fit the API points and data formats!
-
-  let filterData = {};
-
-  let nabozenske_vyznani = [];
-  let narodnosti = [];
-  let mandaty = [];
-  let parlamenty = [];
-  let maFotku = [];
-  let maGalerii = [];
-  let maPrimarniSocialniVazby = [];
-  let maSekundarniSocialniVazby = [];
-
-  let vysoka_skola = [];
-  let maji_vysokou_skolu = [];
-
-  poslanci.forEach((item) => {
-
-    const nabozenstvi_edited = (item.NabozenstviNarozeni === null || Array.isArray(item.NabozenstviNarozeni)) ? 'neuvedeno' : item.NabozenstviNarozeni;
-    const narodnosti_edited = (item.Narodnosti === null || !Array.isArray(item.Narodnosti)) ? 'neuvedeno' : item.Narodnosti;
-
-    const mandaty_edited = (item.Mandaty === null || !Array.isArray(item.Mandaty)) ? 'neuvedeno' : item.Mandaty;
-    const soubory_edited = (item.Soubory === null) ? [] : item.Soubory;
-
-    const vztahy_primarni_edited = (item.OsobniVztahyPrimarni === null) ? [] : item.OsobniVztahyPrimarni;
-    const vztahy_sekundarni_edited = (item.OsobniVztahySekundarni === null) ? [] : item.OsobniVztahySekundarni;
-
-    const parlamenty_edited = (item.Mandaty === null || !Array.isArray(item.Mandaty)) ? 'neuvedeno' : item.Mandaty.map(mandat => mandat.Parlament);
-
-    nabozenske_vyznani.push(nabozenstvi_edited);
-
-    if (Array.isArray(narodnosti_edited)) {
-      narodnosti.push(...narodnosti_edited);
-    } else {
-      narodnosti.push(narodnosti_edited);
-    }
-
-    if (Array.isArray(parlamenty_edited)) {
-      parlamenty.push(...parlamenty_edited);
-    } else {
-      parlamenty.push(parlamenty_edited);
-    }
-
-    maji_vysokou_skolu.push(item.UniverzitniVzdelani);
-
-
-    if (Array.isArray(mandaty_edited )) {
-      mandaty.push(mandaty_edited.length);
-    } else {
-      mandaty.push(0);
-    }
-
-    maFotku.push(soubory_edited.length);
-    maGalerii.push(soubory_edited.length > 1);
-    maPrimarniSocialniVazby.push(vztahy_primarni_edited.length);
-    maSekundarniSocialniVazby.push(vztahy_sekundarni_edited.length);
-
-  });
-
-  // make unique values
-  nabozenske_vyznani = [...new Set(nabozenske_vyznani)].sort((a,b) => a.toString().localeCompare(b));
-  narodnosti = [...new Set(narodnosti)].sort((a,b) => a.toString().localeCompare(b));
-  mandaty = [...new Set(mandaty)];
-  parlamenty = [...new Set(parlamenty)];
-
-  maji_vysokou_skolu = [...new Set(maji_vysokou_skolu)].includes(true);
-
-
-  maFotku = [...new Set(maFotku)];
-  maGalerii = [...new Set(maGalerii)];
-  maPrimarniSocialniVazby = [...new Set(maPrimarniSocialniVazby)];
-  maSekundarniSocialniVazby = [...new Set(maSekundarniSocialniVazby)];
-
-
-  filterData = {
-    pocet: poslanci.length,
-    nabozenske_vyznani,
-    narodnosti,
-    mandaty,
-    parlamenty,
-    maFotku,
-    maGalerii,
-    maPrimarniSocialniVazby,
-    maSekundarniSocialniVazby,
-  };
-
-
-  return filterData;
-
-};
-
+import {
+  normalizeUstrApiMediaObjectForWordpress,
+  normalizeMediaAttrs,
+  getAdresaDruhHumanReadableName,
+  getAdresyProMapuForPoslanec,
+  getCasovaOsaDataForPoslanec,
+  getFilterDataFromPoslanciAll,
+  normalizeSouborAttrs,
+} from '../utils/functions';
 
 export const state = () => ({
   slovnikova_hesla: [],
@@ -515,8 +237,7 @@ export const actions = {
       // we want a specific media id
 
       // check if we have it in the store
-      const storeItems = [...state.media_soubory];
-      const storeItem = storeItems.filter(soubor => soubor.id == opts.id);
+      const storeItem = [...state.media_soubory].filter(soubor => soubor.id == opts.id);
 
       if (storeItem.length && storeItem.length > 0) {
         // return the required item from the store
@@ -532,6 +253,10 @@ export const actions = {
 
           let media_soubory = [...state.media_soubory, post];
 
+          media_soubory = media_soubory.map(item => {
+            return normalizeSouborAttrs(item);
+          });
+
           commit("updateMedia", media_soubory);
           return;
 
@@ -544,7 +269,9 @@ export const actions = {
     } else {
 
       if (state.is_downloaded.media_soubory) {
-        return state.media_soubory;
+
+        return;
+
       } else {
 
         try {
@@ -753,7 +480,11 @@ export const actions = {
 
         if (thisWPParlamentObj.acf && thisWPParlamentObj.acf.galerie) {
 
-          parlament.Galerie = thisWPParlamentObj.acf.galerie;
+          parlament.Galerie = thisWPParlamentObj.acf.galerie.map(item => {
+
+            return normalizeSouborAttrs(item);
+
+          });
 
         }
 
