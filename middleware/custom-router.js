a5ab41969899d0b44996e9310ad66ff27f1d51fb
Jakub Albert Ferenc <jakub.ferenc@spaceflow.io>
2022-12-16 23:16:56 +0100

    remove console logs

--- middleware/custom-router.js
+++ middleware/custom-router.js
@@ -1,7 +1,6 @@
 export default function (context) {
   context.app.router.beforeEach((to, from, next) => {
     context.store.dispatch("searchNavToggle", {searchState: false});
-    console.log('from before each router');
     next();
     return true;
   });
