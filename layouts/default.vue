4d861695d9ca14137591412bac4c96ec0c50b166
Jakub Albert Ferenc <jakub.ferenc@spaceflow.io>
2022-12-16 20:08:37 +0100

    hide debug bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,52 +9,39 @@
 
   <PopupDetail />
 
-  .debug-bar
+  //- .debug-bar
 
 
 </template>
 
-
 <script>
-import Header from '~/components/Header.vue';
-import Main from '~/components/Main.vue';
-import Footer from '~/components/Footer.vue';
-import PopupDetail from '~/components/PopupDetail.vue';
+import Header from "~/components/Header.vue";
+import Main from "~/components/Main.vue";
+import Footer from "~/components/Footer.vue";
+import PopupDetail from "~/components/PopupDetail.vue";
 
 export default {
+  components: { Header, Main, Footer, PopupDetail },
 
-    components: { Header, Main, Footer, PopupDetail },
+  async asyncData({ store }) {},
 
-    async asyncData ({store}) {
+  mounted() {},
 
-    },
-
-    mounted() {
-
-    },
-
-    computed: {
-
-      isPopUpVisible() {
-
-        if (this.$store.state.popup_timeline_detail && this.$store.state.popup_timeline_detail.length) {
-          return Object.keys(this.$store.state.popup_timeline_detail).length > 0;
-        } else {
-          return false;
-        }
-
-
-      }
-
-
-    },
-
-
-    data() {
-      return {
+  computed: {
+    isPopUpVisible() {
+      if (
+        this.$store.state.popup_timeline_detail &&
+        this.$store.state.popup_timeline_detail.length
+      ) {
+        return Object.keys(this.$store.state.popup_timeline_detail).length > 0;
+      } else {
+        return false;
       }
     },
+  },
 
-}
+  data() {
+    return {};
+  },
+};
 </script>
-
