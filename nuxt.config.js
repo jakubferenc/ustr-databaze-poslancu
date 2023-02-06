21d2757b7649f68077bf238a7854c502b1c1c5d9
Jakub Albert Ferenc <jakub.ferenc@spaceflow.io>
2023-02-06 02:01:23 +0100

    add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -212,6 +212,21 @@ export default {
         content: 'my website description',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+    ],
+    link: [
+      {
+        rel: 'icon',
+        sizes: '32x32',
+        type: 'image/x-icon',
+        href: '/favicon-32x32.png',
+      },
+    ],
+    link: [{ rel: 'manifest', href: '/site.webmanifest' }],
   },
 };
